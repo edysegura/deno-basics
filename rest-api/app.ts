d516eb3681dcca8df6d437453f9c3d11e12fab0e
Edy Segura <edysegura@gmail.com>
2025-07-03 17:13:43 -0300

    style: format code to use single quotes and remove unnecessary semicolons

--- rest-api/app.ts
+++ rest-api/app.ts
@@ -1,21 +1,21 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from 'https://deno.land/x/oak/mod.ts'
 
-const router = new Router();
+const router = new Router()
 
 router
   .get('/', ({ response }) => {
-    response.body = 'Server is up and running!';
+    response.body = 'Server is up and running!'
   })
   .get('/hello/:name', ({ params, response }) => {
     response.body = `Hello ${params.name}!`
-  });
+  })
 
-const app = new Application();
-app.use(router.routes());
-app.use(router.allowedMethods());
+const app = new Application()
+app.use(router.routes())
+app.use(router.allowedMethods())
 
-const HOST = Deno.env.get('HOST') || 'localhost';
-const PORT = Deno.env.get('PORT') || 9000;
+const HOST = Deno.env.get('HOST') || 'localhost'
+const PORT = Deno.env.get('PORT') || 9000
 
-console.log(`Server is up and running on http://${HOST}:${PORT}`);
-await app.listen(`${HOST}:${PORT}`);
+console.log(`Server is up and running on http://${HOST}:${PORT}`)
+await app.listen(`${HOST}:${PORT}`)
