5cbf0c7cfa2cf6bbcdd2b8d45c5633731ec033cf
Edy Segura <edysegura@gmail.com>
2025-07-09 14:25:05 -0300

    feat: add HTML file and serve it in the Deno server

--- simple-server/server.ts
+++ simple-server/server.ts
@@ -1,6 +1,9 @@
-Deno.serve((_req: Request) => {
-  return new Response('Hello, World!')
+Deno.serve(async (_req: Request) => {
+  const file = await Deno.readFile('index.html')
+  return new Response(file, {
+    headers: { 'content-type': 'text/html' },
+  })
 })
 
-// deno run --allow-net server.ts
+// deno run --allow-net --allow-read server.ts
 // localhost:8000
